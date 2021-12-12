e30568a278bd88c155b9f111c118d102428c7934
Jamsheelamk <jamssheelamk@gmail.com>
2021-12-12 11:26:39 +0530

    message

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TrainerprofileComponent } from './trainerprofile/trainerprofile.component';
+import { TrainerDashboardComponent } from './trainer-dashboard/trainer-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrainerprofileComponent,
+    TrainerDashboardComponent
   ],
   imports: [
     BrowserModule,
---
+++ Frontend/src/app/trainerprofile/profile.model.ts
@@ -0,0 +1,16 @@
+export class profileModel{
+    constructor(
+    
+    public Name : string,
+    public Email : string,
+    public Phone : string,
+    public Address : string,
+    public HighestQualification : string,
+    public Skillset : string,
+    public CurrentCompanyName : string,
+    public CurrentDesignation : string,
+    public ICTAKCoursesHandling : string,
+    public Photo : string,
+    public ID : string,
+    ){}
+}
