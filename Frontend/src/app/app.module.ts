d0eff573a29fd4db490cf27e499fc6fd3545fd69
arathi <arathiramdas07@gmail.com>
2021-12-12 19:42:44 +0400

    Main page,login,signUp, & edit details done.

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainHomeComponent } from './main-home/main-home.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LoginComponent } from './login/login.component';
+import { EditDetailsFormComponent } from './edit-details-form/edit-details-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHomeComponent,
+    SignUpComponent,
+    LoginComponent,
+    EditDetailsFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
