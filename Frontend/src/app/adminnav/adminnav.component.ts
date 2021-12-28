3ed8909fa8ce757570102054c72589c228433a29
arathi <arathiramdas07@gmail.com>
2021-12-28 17:05:15 +0400

    Logout done

--- Frontend/src/app/adminnav/adminnav.component.ts
+++ Frontend/src/app/adminnav/adminnav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-adminnav',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminnavComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router:Router
+  ) { }
 
   ngOnInit(): void {
   }
-
+  logoutUser(){
+    if(localStorage.getItem('tokenAdmin')){
+      localStorage.removeItem('tokenAdmin');
+    }
+    if(localStorage.getItem('tokenUser')){
+      localStorage.removeItem('tokenUser');
+    }
+    localStorage.removeItem('trainerid')
+    this.router.navigate(['/'])
+  }
 }
