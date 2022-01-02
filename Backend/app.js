d3544f10fbe09d5d43000fe4ba0a14ae415a2826
dev-skas <skasonline@gmail.com>
2022-01-02 15:48:16 +0530

    8

--- Backend/app.js
+++ Backend/app.js
@@ -7,7 +7,7 @@ var nodemailer = require('nodemailer');
 const port = process.env.PORT || 3000;
 const trainerData = require('./src/model/signupModel');  // This is the model containing trainer sign up data
 // MULTER:
-const DIR = '/api/assets/uploads';
+const DIR = '/assets/uploads';
 const path = require('path');
 app.use(express.static('./dist/Frontend'));
 const storage = multer.diskStorage({
