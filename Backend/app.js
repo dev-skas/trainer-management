66cc1b9a49dccd01855ef4acf72b99fa86cc8a8b
Arathi-Ram <52126583+Arathi-Ram@users.noreply.github.com>
2022-01-08 11:31:58 +0400

    Update app.js
    updated /allocate api to save allocation data to db

--- Backend/app.js
+++ Backend/app.js
@@ -126,31 +126,28 @@ app.put('/api/approve',verifyToken,(req,res)=>{   //aprrove trainers
   
         app.post('/api/allocate', verifyToken, function (req, res) {     //allocate trainers
           console.log(req.file);
-          let id = req.body._id;
-          courseid = req.body.courseid,
-            batchid = req.body.batchid,
-            scheduletime = req.body.scheduletime,
-            startdate = req.body.startdate,
-            enddate = req.body.enddate,
-            venue = req.body.venue,
-
-            trainerData.findByIdAndUpdate({ "_id": id }, {
-              $set: {
-                "courseid": courseid,
-                "batchid": batchid,
-                "scheduletime": scheduletime,
-                "startdate": startdate,
-                "enddate": enddate,
-                "venue": venue,
-                "isAllocated": "true" //employment type
-
-              }
-            })
-              .then(function () {
-                res.send()
-                allocatemail(id)
-              })
+            let id=req.body._id;
+            var allocationDetails = allocationData({
+                Unique_ID:req.body.Unique_ID,
+                name:req.body.name,
+                courseid:req.body.courseid,
+                courses:req.body.courses,
+                skillSet:req.body.skillSet,
+                batchid:req.body.batchid,
+                scheduletime:req.body.scheduletime,
+                startdate:req.body.startdate,
+                enddate:req.body.enddate,
+                venue:req.body.venue
         })
+        try{
+             allocationDetails = allocationDetails.save();
+             res.send();
+               allocatemail(id);
+            }
+        catch(err){
+            console.error("Error from Backend(Allocate) = "+err);
+        }
+        });
 
         app.get('/api/allocationDetails/:id', (req, res) => {
           let Unique_ID = req.params.id;
